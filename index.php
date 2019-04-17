611c3bf02ddd322475410d0ca0ed61d2590b439a
Mac <mac@Naveneeta.local>
2019-04-17 15:59:47 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// die("Server upgrade in progress.");
+
+/*
+
+error_reporting(E_ERROR);
+*/
+ini_set("display_errors", 1);
+define("SECURE", true);
+include "library/prehead.php";
+
+//echo SITE_ID;
